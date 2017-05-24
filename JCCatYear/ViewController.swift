17e578696e8d30b4362cab0b81c957d2154923ed
prajakta <prajakta@iternia.com>
2017-05-24 12:26:06 +0530

    Initial Commit

--- JCCatYear/ViewController.swift
+++ JCCatYear/ViewController.swift
@@ -8,8 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet var textAge: UITextField!
+    
+    @IBOutlet var labelDisplay: UILabel!
+    var ageInt:Double = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +26,64 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func buttonCalculate(_ sender: UIButton) {
+       
+        let currentText: String = textAge.text!
+       
+        if  ((textAge.text?.isEmpty) != false) {
+            self .alertAction(Title: "Text Empty", Massage: "Please Enter The Age.")
+            
+        } else {
+            
+            ageInt = Double(currentText)!
+            
+            if ageInt > 0 && ageInt < 100 {
+                
+                ageInt = ageInt / 7
+                
+              
+                let newAgeInt = String(format: "Cat Year Age : %.2f", arguments: [ageInt])
+
+                labelDisplay.text = newAgeInt
+                print(newAgeInt)
+                self.alertAction(Title: newAgeInt, Massage: "Cat Year Age")
+           
+            }else {
+                
+                labelDisplay.text = "Humman Age Is Lessthan 100"
+                self .alertAction(Title: "Worng Value", Massage: "Humman Age Is Lessthan 100")
+                
+            }
+        }
+    
+    
+    }
+    
+    @IBAction func buttonClear(_ sender: UIButton) {
+        labelDisplay.text! = " "
+        textAge.text! = " "
+        
+    }
+    
+    func alertAction(Title:String,Massage:String) {
+        
+        let alert = UIAlertController(title: Title, message: Massage, preferredStyle: .alert)
+        
+        let okAction = UIAlertAction (title: "ok", style: .default) { (action:(UIAlertAction)) in
+            
+        }
+        alert.addAction(okAction)
+
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField .resignFirstResponder()
+        return true
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        self.view .endEditing(true)
+    }
 }
 
